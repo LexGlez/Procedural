ffdd4af88c23b93e1188343f545ff05a45f6d88e
Alex Gonzalez <alexis-axis@hotmail.com>
2021-06-15 19:33:52 -0500

    primer unity subido

---
+++ Assets/procedura.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class procedura : MonoBehaviour
+{
+    public GameObject camino;
+    int a;
+    void Start()
+    {
+        
+    }
+
+    void Update(){}
+
+    public void proceso(){
+        
+        a+=9;
+        Instantiate(camino, new Vector3(0, 0, a), Quaternion.identity);
+    }
+}
